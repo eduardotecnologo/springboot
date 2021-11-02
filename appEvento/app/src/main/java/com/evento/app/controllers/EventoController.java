bb93dc20f597d332ce3fcd60125496870832f8db
eduardotecnologo <eduardotecnologo@hotmail.com>
2021-11-01 17:42:48 -0700

    install Materialize

--- appEvento/app/src/main/java/com/evento/app/controllers/EventoController.java
+++ appEvento/app/src/main/java/com/evento/app/controllers/EventoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.evento.app.models.Evento;
 import com.evento.app.repository.EventoRepository;
@@ -25,4 +26,19 @@ public class EventoController {
 		
 		return "redirect:/cadastrarEvento";
 	}
+	
+	@RequestMapping("/eventos")
+	public ModelAndView listaEventos() {
+		ModelAndView mv = new ModelAndView("index");
+		Iterable<Evento> eventos = er.findAll();
+		mv.addObject("eventos", eventos);
+		return mv;
+	}
 }
+	
+	
+	
+	
+	
+	
+	
